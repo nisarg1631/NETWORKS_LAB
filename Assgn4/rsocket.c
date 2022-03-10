d1b9f6984f1f2aeb7341ff47577884bdeee0c280
animesh <jha.animesh01@gmail.com>
2022-03-10 15:25:15 +0530

    skeleton

---
+++ Assgn4/rsocket.c
@@ -0,0 +1,2 @@
+#include "rsocket.h"
+pthread_mutex_t mutex_ptr_rtable, mutex_ptr_utable;
--- Assgn4/rsocket.h
+++ Assgn4/rsocket.h
@@ -1,3 +1,20 @@
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <pthread.h>
+#include <time.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <sys/time.h>
+#include <arpa/inet.h>
+
+float drop_prob = 0.1;
+const int SOCK_MRP = 42;
+
+int r_socket(int domain, int type, int protocol);
+int r_bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
+ssize_t r_sendto(int sockfd, const void *buf, size_t len, int flags, const struct sockaddr *dest_addr, socklen_t addrlen);
+ssize_t r_recvfrom(int sockfd, void *buf, size_t len, int flags, struct sockaddr *src_addr, socklen_t *addrlen);
+int r_close(int fd);
+int dropMessage(float p);
