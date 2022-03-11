1e0e844a937facf1897bdef1291c8ebf624f2340
animesh <jha.animesh01@gmail.com>
2022-03-11 08:45:29 +0530

    brrrrrrrrrrr

--- Assgn4/rsocket.c
+++ Assgn4/rsocket.c
@@ -1,33 +1,91 @@
 #include "rsocket.h"
 pthread_mutex_t mutex_ptr_rtable, mutex_ptr_utable;
-
-// The message tables are the simplest possible data structures because at one point no more than 50 msgs will be in them,
-// So it seems to me that the overhead of complicated data structures is not worth it. something like a Binary Search Tree can be well suited for this task , however the constatns int O(logn) would be more than the time for a naive array based solution 
-// even though the array based solution might not be asymptotically optimal.
-// Unacknowledged message table
-typedef struct
-{
-    int msg_id;
-    struct sockaddr_in dest_addr;
-    char msg_body[52];
-    time_t t_sent;
-} umsg;
-// Received message table
-typedef struct
-{
-    int msg_id;
-    int msg_type;
-    struct sockaddr_in src_addr;
-    char msg_body[52];
-} rmsg;
-umsg *utable;
-rmsg *rtable;
-int add_to_utable();
-int add_to_rtable();
-int remove_from_utable();
-int remove_from_rtable();
-int sock = -1;
 const int BUF_SIZE = 52;
+utable *unack_table = NULL;
+rtable *recv_table = NULL;
+int last_used_msg_id = 0;
+void init_utable(utable *u, int s)
+{
+    pthread_mutex_lock(&mutex_ptr_utable);
+    u->next_to_use = 0;
+    u->size = s;
+    u->arr = (umsg *)calloc(s, sizeof(umsg));
+    pthread_mutex_unlock(&mutex_ptr_utable);
+}
+int add_to_utable(utable *u, umsg *msg)
+{
+    pthread_mutex_lock(&mutex_ptr_utable);
+    if (u->next_to_use == u->size)
+    {
+        pthread_mutex_unlock(&mutex_ptr_utable);
+        return 0;
+    }
+    u->arr[u->next_to_use] = *msg;
+    u->next_to_use++;
+    pthread_mutex_unlock(&mutex_ptr_utable);
+    return 1;
+};
+int remove_from_utable(utable *u, umsg *msg)
+{
+    pthread_mutex_lock(&mutex_ptr_utable);
+    for (int i = 0; i < u->next_to_use; i++)
+    {
+        if (u->arr[i].msg_id == msg->msg_id)
+        {
+            for (int j = i; j + 1 < u->next_to_use; j++)
+            {
+                u->arr[j] = u->arr[j + 1];
+            }
+            u->next_to_use--;
+            pthread_mutex_unlock(&mutex_ptr_utable);
+
+            return 1;
+        }
+    }
+    pthread_mutex_unlock(&mutex_ptr_utable);
+    return 0;
+};
+void init_rtable(rtable *r, int s)
+{
+    pthread_mutex_lock(&mutex_ptr_rtable);
+    r->next_to_use = 0;
+    r->size = s;
+    r->arr = (rmsg *)calloc(s, sizeof(rmsg));
+    pthread_mutex_unlock(&mutex_ptr_rtable);
+}
+int add_to_rtable(rtable *r, rmsg *msg)
+{
+    pthread_mutex_lock(&mutex_ptr_rtable);
+
+    if (r->next_to_use == r->size)
+    {
+        pthread_mutex_unlock(&mutex_ptr_rtable);
+        return 0;
+    }
+    r->arr[r->next_to_use] = *msg;
+    r->next_to_use++;
+    pthread_mutex_unlock(&mutex_ptr_rtable);
+    return 1;
+}
+int remove_from_rtable(rtable *r, rmsg *msg)
+{
+    pthread_mutex_lock(&mutex_ptr_rtable);
+    for (int i = 0; i < r->next_to_use; i++)
+    {
+        if (r->arr[i].msg_id == msg->msg_id)
+        {
+            for (int j = i; j + 1 < r->next_to_use; j++)
+            {
+                r->arr[j] = r->arr[j + 1];
+            }
+            r->next_to_use--;
+            pthread_mutex_unlock(&mutex_ptr_rtable);
+            return 1;
+        }
+    }
+    pthread_mutex_unlock(&mutex_ptr_rtable);
+    return 0;
+}
 int r_socket(int domain, int type, int protocol)
 {
     if (type ^ SOCK_MRP)
@@ -37,15 +95,25 @@ int r_socket(int domain, int type, int protocol)
 
     srand(time(NULL));
     // create tables
-    utable = (umsg *)calloc(50, sizeof(umsg));
-    rtable = (rmsg *)calloc(50, sizeof(rmsg));
+    unack_table = (utable *)calloc(1, sizeof(utable));
+    recv_table = (rtable *)calloc(1, sizeof(rtable));
+    init_utable(unack_table, 75);
+    init_rtable(recv_table, 75);
+
+    // create mutexes
+    pthread_mutex_init(&mutex_ptr_rtable, NULL);
+    pthread_mutex_init(&mutex_ptr_utable, NULL);
+
+    // create socket
+    int sock = socket(domain, SOCK_DGRAM, protocol);
+
     // create thread r and s
     pthread_t R, S;
     pthread_attr_t attr;
     pthread_attr_init(&attr);
-    pthread_create(R, &attr, run_thread_r, NULL);
-    pthread_create(S, &attr, run_thread_s, NULL);
-    sock = socket(domain, SOCK_DGRAM, protocol);
+    pthread_create(R, &attr, run_thread_r, (void *)&sock);
+    pthread_create(S, &attr, run_thread_s, (void *)&sock);
+
     return sock;
 }
 int r_bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen)
@@ -54,12 +122,30 @@ int r_bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen)
 }
 ssize_t r_sendto(int sockfd, const void *buf, size_t len, int flags, const struct sockaddr *dest_addr, socklen_t addrlen)
 {
-    ssize_t ret = sendto(sockfd, buf, len, flags, dest_addr, addrlen);
-    add_to_utable(buf, len, dest_addr);
+    char *buf_ = (char *)calloc(len + 5, sizeof(char));
+    // Type of msg
+    // Msg id  (assumed max 4 digits)
+    buf_[0] = Data_msg;
+    int n = last_used_msg_id;
+    for (int i = 0; i < 4; i++)
+    {
+        buf_[4 - i] = (n % 10) + '0';
+        n /= 10;
+    }
+    strcpy(buf_ + 5, buf);
+    ssize_t ret = sendto(sockfd, buf_, len + 5, flags, dest_addr, addrlen);
+    umsg *msg = (umsg *)calloc(1, sizeof(umsg));
+    // create msg here
+    msg->msg_id = last_used_msg_id++;
+    msg->t_sent = time(0);
+    msg->dest_addr = dest_addr;
+    strcpy(msg->msg_body, buf);
+    add_to_utable(unack_table, msg);
+    return ret;
 }
 ssize_t r_recvfrom(int sockfd, void *buf, size_t len, int flags, struct sockaddr *src_addr, socklen_t *addrlen)
 {
-    if () // rtable is non empty
+    if (recv_table->next_to_use > 0) // rtable is non empty
     {
         pthread_mutex_lock(&mutex_ptr_rtable);
         // take the first message in rtable
@@ -82,6 +168,8 @@ void run_thread_r(void *param)
     //  received-message table, and sends an ACK message to the sender. If it is an ACK message
     //  in response to a previously sent message, it updates the unacknowledged-message table to
     //  take out the message for which the acknowledgement has arrived.
+    int *sockptr = (int *)param;
+    int sock = *sockptr;
     char buf[BUF_SIZE] = {0};
     while (1)
     {
@@ -104,10 +192,8 @@ void run_thread_r(void *param)
         // if it is an ack message,
         else if ()
         {
-            pthread_mutex_lock(&mutex_ptr_utable);
             //  remove from utable
             remove_from_utable();
-            pthread_mutex_unlock(&mutex_ptr_utable);
         }
         else
         {
--- Assgn4/rsocket.h
+++ Assgn4/rsocket.h
@@ -18,3 +18,40 @@ ssize_t r_sendto(int sockfd, const void *buf, size_t len, int flags, const struc
 ssize_t r_recvfrom(int sockfd, void *buf, size_t len, int flags, struct sockaddr *src_addr, socklen_t *addrlen);
 int r_close(int fd);
 int dropMessage(float p);
+
+// The message tables are the simplest possible data structures because at one point no more than 50 msgs will be in them,
+// So it seems to me that the overhead of complicated data structures is not worth it. something like a Binary Search Tree can be well suited for this task , however the constatns int O(logn) would be more than the time for a naive array based solution
+// even though the array based solution might not be asymptotically optimal.
+// Unacknowledged message table
+typedef struct
+{
+    int msg_id;
+    struct sockaddr *dest_addr;
+    char msg_body[52];
+    time_t t_sent;
+} umsg;
+// Received message table
+typedef struct
+{
+    int msg_id;
+    int msg_type;
+    struct sockaddr *src_addr;
+    char msg_body[52];
+} rmsg;
+
+typedef struct
+{
+    // array
+    int size;
+    int next_to_use;
+    umsg *arr;
+} utable;
+typedef struct
+{
+    // array
+    int size;
+    int next_to_use;
+    rmsg *arr;
+} rtable;
+const int ACK_msg = 1;
+const int Data_msg = 0;
